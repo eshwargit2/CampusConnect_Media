f89d60166d55b12dbf59389dc733f0ac9cc779ab
eshwargit2 <msselectronic57@gmail.com>
2026-03-08 10:01:23 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: process.env.NODE_ENV === 'production'
-        ? ['https://www.collegecircle.app']
-        : ['http://localhost:5173', 'http://localhost:3000'],
+    origin: true,   // reflect any origin
     credentials: true,
 }));
 
